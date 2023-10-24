c9ea02b6949cec928e062e90761f1cb83043e699
Zhikun Chen <2612803c@student.gla.ac.uk>
2023-10-25 03:51:48 +0800

    init2

--- FeedbackMate/FeedbackMate/urls.py
+++ FeedbackMate/FeedbackMate/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from main import views
 
+
 urlpatterns = [
-    #path("admin/", admin.site.urls),
-    path('index/',views.index),
+    path('admin', admin.site.urls),
+    path('index/', views.index),
+    path('login/', views.login),
+    path('register/', views.registerPage, name="register")
 ]
+
